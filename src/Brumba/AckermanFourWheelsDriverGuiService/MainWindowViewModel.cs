7e75a2423bc1a72d443935656ee689c4f7994c11
matvey <matvey@cf9289f8-4b28-4085-b22d-9969776bd1f6>
2012-12-18 11:50:06 +0000

    CLEANED unused references
    git-svn-id: svn://brumba.ru:46104/brumba@18 cf9289f8-4b28-4085-b22d-9969776bd1f6

--- src/Brumba/AckermanFourWheelsDriverGuiService/MainWindowViewModel.cs
+++ src/Brumba/AckermanFourWheelsDriverGuiService/MainWindowViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Brumba.Simulation.AckermanFourWheelsDriverGuiService
+﻿namespace Brumba.Simulation.AckermanFourWheelsDriverGuiService
 {
     public class MainWindowViewModel
     {
