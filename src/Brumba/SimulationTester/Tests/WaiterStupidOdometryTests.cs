b2596088054f86bd400a11e64f942b83128bf9a0
matvey <matvey@cf9289f8-4b28-4085-b22d-9969776bd1f6>
2013-11-24 20:16:16 +0000

    IMPLEMENTED Odometry RotateInPlace test
    git-svn-id: svn://brumba.ru:46104/brumba@92 cf9289f8-4b28-4085-b22d-9969776bd1f6

--- src/Brumba/SimulationTester/Tests/WaiterStupidOdometryTests.cs
+++ src/Brumba/SimulationTester/Tests/WaiterStupidOdometryTests.cs
@@ -8,17 +8,14 @@ using Microsoft.Robotics.PhysicalModel;
 using Microsoft.Robotics.Simulation.Engine;
 using Microsoft.Robotics.Simulation.Physics;
 using Microsoft.Xna.Framework;
-using Quaternion = Microsoft.Robotics.PhysicalModel.Quaternion;
 using Vector3 = Microsoft.Xna.Framework.Vector3;
 using drivePxy = Microsoft.Robotics.Services.Drive.Proxy;
-using lrfPxy = Microsoft.Robotics.Services.Sensors.SickLRF.Proxy;
 using engPxy = Microsoft.Robotics.Simulation.Engine.Proxy;
 using odometryPxy = Brumba.WaiterStupid.Odometry.Proxy;
-using VisualEntityPxy = Microsoft.Robotics.Simulation.Engine.Proxy.VisualEntity;
 
 namespace Brumba.Simulation.SimulationTester.Tests
 {
-	[SimTestFixture("waiter_stupid_odometry_tests")]
+	[SimTestFixture("waiter_stupid_odometry_tests", Wip = true)]
 	public class WaiterStupidOdometryTests
 	{
 		public ServiceForwarder ServiceForwarder { get; private set; }
@@ -67,9 +64,9 @@ namespace Brumba.Simulation.SimulationTester.Tests
 				var poseDifference = odometryState.State.Pose - SimPoseToEgocentricPose(simPose);
                 //Console.WriteLine("From Odometry ******{0}", odometryState.State.Pose);
                 //Console.WriteLine("From Simulation ****{0}", SimPoseToEgocentricPose(simPose));
-                //Console.WriteLine("Ratio **************{0}", poseDifference.Length() / odometryState.State.Pose.Length());
+                //Console.WriteLine("Ratio **************{0}", poseDifference.Length() / SimPoseToEgocentricPose(simPose).Length());
 
-				@return(!(_failed = poseDifference.Length() / odometryState.State.Pose.Length() > 0.05));
+                @return(!(_failed = poseDifference.Length() / SimPoseToEgocentricPose(simPose).Length() > 0.05));
 			}
 
 			static Vector3 SimPoseToEgocentricPose(Pose pose)
@@ -78,44 +75,49 @@ namespace Brumba.Simulation.SimulationTester.Tests
 			}
 		}
 
-		//[SimTest]
+		[SimTest]
 		public class RotateInPlace : StochasticTest
 		{
+            private bool _failed;
+
 			public override void PrepareForReset(VisualEntity entity)
 			{
 			}
 
 			public override IEnumerator<ITask> Start()
 			{
+                _failed = false;
 				EstimatedTime = 5;
 
 				//Execs for synchronization, otherwise set power message can arrive before enable message
 				yield return To.Exec((Fixture as WaiterStupidOdometryTests).RefPlDrivePort.EnableDrive(true));
-				yield return To.Exec((Fixture as WaiterStupidOdometryTests).RefPlDrivePort.SetDrivePower(-1.0, 1.0));
+				yield return To.Exec((Fixture as WaiterStupidOdometryTests).RefPlDrivePort.SetDrivePower(-0.2, 0.2));
 			}
 
 			public override IEnumerator<ITask> AssessProgress(Action<bool> @return, IEnumerable<engPxy.VisualEntity> simStateEntities, double elapsedTime)
 			{
-				if (elapsedTime < EstimatedTime - 1)
-				{
-					@return(false);
-					yield break;
-				}
+                if (elapsedTime < 0.9 * EstimatedTime || _failed)
+                {
+                    @return(false);
+                    yield break;
+                }
 
 				var simPose = (Pose)DssTypeHelper.TransformFromProxy(simStateEntities.Single(epxy => epxy.State.Name == "stupid_waiter@").State.Pose);
 				odometryPxy.OdometryServiceState odometryState = null;
 				yield return (Fixture as WaiterStupidOdometryTests).OdometryPort.Get().Receive(os => odometryState = os);
 
-				var poseDifference = odometryState.State.Pose - SimPoseToEgocentricPose(simPose);
-				Console.WriteLine("From Odometry ******{0}", odometryState.State.Pose);
-				Console.WriteLine("From Simulation ****{0}", SimPoseToEgocentricPose(simPose));
-				Console.WriteLine("Ratio **************{0}", poseDifference.Length() / odometryState.State.Pose.Length());
-				@return(poseDifference.Length() / odometryState.State.Pose.Length() <= 0.01);
+				var thetaDifference = Math.Abs(odometryState.State.Pose.Z - SimPoseToEgocentricPose(simPose).Z);
+                thetaDifference = Math.Min(thetaDifference, MathHelper.TwoPi - thetaDifference);
+				Console.WriteLine("From Odometry ******{0}", odometryState.State.Pose.Z);
+				Console.WriteLine("From Simulation ****{0}", SimPoseToEgocentricPose(simPose).Z);
+                Console.WriteLine("Ratio **************{0}", thetaDifference / MathHelper.TwoPi);
+
+                @return(!(_failed = thetaDifference / MathHelper.TwoPi > 0.05));
 			}
 
 			static Vector3 SimPoseToEgocentricPose(Pose pose)
 			{
-				return new Vector3(-pose.Position.Z, pose.Position.X, UIMath.QuaternionToEuler(pose.Orientation).X);
+                return new Vector3(-pose.Position.Z, pose.Position.X, MathHelper.ToRadians(UIMath.QuaternionToEuler(pose.Orientation).Y));
 			}
 		}
 	}
