9e6464581dc77b906fe7d610def686c93057b8b3
dron <dron@cf9289f8-4b28-4085-b22d-9969776bd1f6>
2014-03-16 20:26:56 +0000

    git-svn-id: svn://brumba.ru:46104/brumba@140 cf9289f8-4b28-4085-b22d-9969776bd1f6

---
+++ src/Brumba/WaiterStupid/Gui/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using MathNet.Numerics.LinearAlgebra.Double;
+using Matrix = MathNet.Numerics.LinearAlgebra.Double.Matrix;
+
+namespace Brumba.WaiterStupid.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            HeatMapVm = new HeatMapViewModel();         
+        }
+
+        public HeatMapViewModel HeatMapVm { get; set; }
+
+        public void ShowMatrix(Matrix matrix)
+        {            
+            HeatMapVm.UpdateHeatMap(matrix);
+        }
+
+        public void InitVisual(string matrixName, Color? minColor = null, Color? maxColor = null)
+        {
+            HeatMapVm.InitVisual(matrixName,minColor,maxColor);
+        }
+    }
+
+}
+
