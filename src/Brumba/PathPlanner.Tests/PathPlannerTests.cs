f866af3f9629904723dfc03a157ea124f89b9d7e
matvey <matvey@cf9289f8-4b28-4085-b22d-9969776bd1f6>
2014-07-25 06:31:51 +0000

    IMPLEMENTING path planner
    git-svn-id: svn://brumba.ru:46104/brumba@182 cf9289f8-4b28-4085-b22d-9969776bd1f6

--- src/Brumba/PathPlanner.Tests/PathPlannerTests.cs
+++ src/Brumba/PathPlanner.Tests/PathPlannerTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using Brumba.DsspUtils;
 using Brumba.MapProvider;
 using Brumba.McLrfLocalizer;
 using MathNet.Numerics;
@@ -18,18 +19,26 @@ namespace Brumba.PathPlanner.Tests
         [Test]
         public void Acceptance()
         {
-            //var map = MapProviderService.CreateOccupancyGrid((Bitmap)Image.FromFile("simple_house.bmp"),
-                //0.1, new[] { Color.Black }, Color.White);
+            var map = MapProviderService.CreateOccupancyGrid((Bitmap)Image.FromFile("simple_house.bmp"),
+                0.1, new[] { Color.Black }, Color.White);
 
-            //var pp = new PathPlanner(map);
+            var pp = new PathPlanner(map: map, robotDiameter: 0.2f);
+
+            var checkPoints = pp.Plan(from: new Vector2(0.1f, 3.5f), to: new Vector2(6, 2));
+
+            Assert.That(checkPoints, Is.EquivalentTo(new [] {new Vector2(), new Vector2(), new Vector2()}));
         }
     }
 
-    public class PathPlanner
+    class PathPlanner
     {
-        public static OccupancyGrid InflateMap(OccupancyGrid map, double delta)
+        public PathPlanner(OccupancyGrid map, float robotDiameter)
         {
-            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Vector2> Plan(Vector2 from, Vector2 to)
+        {
+            throw new NotImplementedException();
         }
     }
 }
