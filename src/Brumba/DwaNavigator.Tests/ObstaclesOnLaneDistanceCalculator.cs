c097b91850928ddb7f8960349ee3d1acc7685a34
Matvey Stepanov <matthewz@yandex.ru>
2014-10-23 13:52:54 +0400

    REFACTORED ObstaclesOnLaneDistanceCalculator

---
+++ src/Brumba/DwaNavigator.Tests/ObstaclesOnLaneDistanceCalculator.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MathNet.Numerics;
+using Microsoft.Xna.Framework;
+
+namespace Brumba.DwaNavigator.Tests
+{
+    public class ObstaclesOnLaneDistanceCalculator
+    {
+        private readonly double _robotRadius;
+
+        public ObstaclesOnLaneDistanceCalculator(double robotRadius)
+        {
+            _robotRadius = robotRadius;
+        }
+
+        public double GetDistanceToClosestObstacle(double linearVelocity, double angularVelocity, IEnumerable<Vector2> obstacles)
+        {
+            return (angularVelocity == 0
+                ? DistancesToObstaclesOnLine(obstacles)
+                : DistancesToObstaclesOnCircle(linearVelocity / angularVelocity, obstacles)).
+                DefaultIfEmpty(float.PositiveInfinity).Min() - _robotRadius;
+        }
+
+        public IEnumerable<double> DistancesToObstaclesOnLine(IEnumerable<Vector2> obstacles)
+        {
+            //contracts: linearVelocity > 0
+            //ensures > 0
+
+            //obstacles are in robot coordinates
+            return obstacles.
+                Where(o => o.Y <= _robotRadius && o.Y >= -_robotRadius && o.X >= 0).
+                Select(o => (double)o.X);
+        }
+
+        public IEnumerable<double> DistancesToObstaclesOnCircle(double circleRadius, IEnumerable<Vector2> obstacles)
+        {
+            var circleCenter = new Vector2(0, (float)circleRadius);
+
+            return obstacles.
+                Where(o => 
+                    PointIsInCircle(false, circleCenter, o, Math.Abs(circleRadius) - _robotRadius) &&
+                    PointIsInCircle(true, circleCenter, o, Math.Abs(circleRadius) + _robotRadius)).
+                Select(o =>
+                {
+                    var angularDistanceAlongCircle = Math.Acos(Vector2.Dot(Vector2.Normalize(o - circleCenter), Vector2.Normalize(-circleCenter)));
+                    return Math.Abs(circleRadius) * (o.X < 0 ? Constants.Pi2 - angularDistanceAlongCircle : angularDistanceAlongCircle);
+                });
+        }
+
+        static bool PointIsInCircle(bool inside, Vector2 curveCenter, Vector2 point, double radius)
+        {
+            Func<double, double, bool> InsideOrOutside = (a, b) => inside ? a <= b : a >= b;
+            return InsideOrOutside((point.X - curveCenter.X) * (point.X - curveCenter.X) + (point.Y - curveCenter.Y) * (point.Y - curveCenter.Y), radius * radius);
+        }
+    }
+}
---
+++ src/Brumba/DwaNavigator.Tests/ObstaclesOnLaneDistanceCalculatorTests.cs
@@ -0,0 +1,78 @@
+using System.Linq;
+using MathNet.Numerics;
+using Microsoft.Xna.Framework;
+using NUnit.Framework;
+
+namespace Brumba.DwaNavigator.Tests
+{
+    [TestFixture]
+    public class ObstaclesOnLaneDistanceCalculatorTests
+    {
+        [Test]
+        public void DistancesToObstaclesOnLine()
+        {
+            var dc = new ObstaclesOnLaneDistanceCalculator(1);
+
+            //No obstacles
+            Assert.That(dc.DistancesToObstaclesOnLine(obstacles: new Vector2[0]), Is.Empty);
+
+            //Obstacles are not in the lane
+            Assert.That(dc.DistancesToObstaclesOnLine(new[] { new Vector2(0, 10), new Vector2(0, -10), new Vector2(-3, 0) }), Is.Empty);
+
+            //One obstacle staright ahead
+            Assert.That(dc.DistancesToObstaclesOnLine(new[] { new Vector2(3, 0) }).Single(), Is.EqualTo(3));
+
+            //One obstacle ahead and to the side
+            Assert.That(dc.DistancesToObstaclesOnLine(new[] { new Vector2(3, 0.5f) }).Single(), Is.EqualTo(3));
+
+            //Obstacle is exactly on the border of the lane
+            Assert.That(dc.DistancesToObstaclesOnLine(new[] { new Vector2(3, 1) }).Single(), Is.EqualTo(3));
+            
+            //Two obstacles ahead, one is closer
+            Assert.That(dc.DistancesToObstaclesOnLine(new[] { new Vector2(3, 0.5f), new Vector2(4, -0.5f) }), Is.EquivalentTo(new [] {3d, 4}));
+        }
+
+        [Test]
+        public void DistancesToObstaclesOnCircle()
+        {
+            var dc = new ObstaclesOnLaneDistanceCalculator(1);
+
+            //No obstacles
+            Assert.That(dc.DistancesToObstaclesOnCircle(circleRadius: 5, obstacles: new Vector2[0]), Is.Empty);
+
+            //Obstacle is not on the lane
+            Assert.That(dc.DistancesToObstaclesOnCircle(5, new[] { new Vector2(10, 10) }), Is.Empty);
+
+            //Obstacle is exactly on X axis
+            Assert.That(dc.DistancesToObstaclesOnCircle(5, new[] { new Vector2(0, 10) }).Single(), Is.EqualTo(Constants.Pi * 5));
+            //Obstacle on positive part of the lane
+            Assert.That(dc.DistancesToObstaclesOnCircle(5, new[] { new Vector2(0.1f, 10) }).Single(), Is.LessThan(Constants.Pi * 5));
+            //Obstacle on negative part of the lane
+            Assert.That(dc.DistancesToObstaclesOnCircle(5, new[] { new Vector2(-0.1f, 10) }).Single(), Is.GreaterThan(Constants.Pi * 5));
+            //Obstacle is exactly on the border of lane
+            Assert.That(dc.DistancesToObstaclesOnCircle(5, new[] { new Vector2(0, 11) }).Single(), Is.EqualTo(Constants.Pi * 5));
+            //Obstacle on positive part of the lane, but has negative Y
+            Assert.That(dc.DistancesToObstaclesOnCircle(5, new[] { new Vector2(1.1f, -0.05f) }).Single(), Is.GreaterThan(0));
+            //Curve down
+            Assert.That(dc.DistancesToObstaclesOnCircle(-5, new[] { new Vector2(0, -10) }).Single(), Is.EqualTo(Constants.Pi * 5));
+            //Two obstacles are exactly on X axis
+            Assert.That(dc.DistancesToObstaclesOnCircle(5, new[] {new Vector2(0, 9), new Vector2(0, 11)}),
+                Is.EquivalentTo(new[] {Constants.Pi * 5, Constants.Pi * 5}));
+        }
+
+        [Test]
+        public void GetDistanceToClosestObstacle()
+        {
+            var dc = new ObstaclesOnLaneDistanceCalculator(robotRadius: 1d);
+
+            //One obstacle staright ahead, moving on line
+            Assert.That(double.IsPositiveInfinity(dc.GetDistanceToClosestObstacle(50, 0, new Vector2[0])));
+
+            //One obstacle staright ahead, moving on line
+            Assert.That(dc.GetDistanceToClosestObstacle(50, 0, new[] { new Vector2(3, 0) }), Is.EqualTo(2));
+
+            //Obstacle is exactly on X axis, moving on circle
+            Assert.That(dc.GetDistanceToClosestObstacle(50, 10, new[] { new Vector2(0, 10) }), Is.EqualTo(Constants.Pi * 5 - 1));
+        }
+    }
+}
