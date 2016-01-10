1b59fd99f4acc7c11df55775f00d4e8c787e4643
Matvey Stepanov <matthewz@yandex.ru>
2016-01-10 23:20:01 +0300

    REFACTORED TimerTypes moved to DsspUtils from AckermanVehicle

--- src/Brumba/DsspUtils/Properties/AssemblyInfo.cs
+++ src/Brumba/DsspUtils/Properties/AssemblyInfo.cs
@@ -1,6 +1,9 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using dss = Microsoft.Dss.Core.Attributes;
+
+[assembly: dss.ServiceDeclaration(dss.DssServiceDeclaration.ServiceBehavior)]
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
